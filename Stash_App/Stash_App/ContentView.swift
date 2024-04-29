216d8e3deb3af6ab32384b60dba5c13d4ff28d1a
Matt Deuschle <mdeuschle@gmail.com>
2024-04-28 21:03:38 -0700

    first commit

---
+++ Stash_App/Stash_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stash_App
+//
+//  Created by Matt Deuschle on 4/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stash_App/Stash_App/Stash_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stash_AppApp.swift
+//  Stash_App
+//
+//  Created by Matt Deuschle on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Stash_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
